e77b0f8279b7caadc5e3ea40985f0e8b929cb7e1
xyz987164 <xyz987164@gmail.com>
2025-06-30 13:11:37 +0900

    feat: 초기 설정 파일

---
+++ icey/src/main/java/com/project/icey/IceyApplication.java
@@ -0,0 +1,13 @@
+package com.project.icey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceyApplication.class, args);
+	}
+
+}
