a0de15d0b9a099eda4a0fa71520ae32dae9acbc9
yoonjae-kwon <yoonjae.kwon03@gmail.com>
2025-07-07 20:00:49 +0900

    Revert "User 관련 오류 수정+색 관련 코드 수정"
    This reverts commit 8009e701aba7c3b270112b545e6c0b9d90eaeffc.

--- icey/src/main/java/com/project/icey/app/dto/CardResponse.java
+++ icey/src/main/java/com/project/icey/app/dto/CardResponse.java
@@ -5,13 +5,8 @@ import lombok.Data;
 
 @Data @AllArgsConstructor
 public class CardResponse {
-    private Long cardId; // 카드 ID. 템플릿도 유저도 아님
+    private Long id;
     private String nickname;
     private String animal;
     private String profileColor;
-    private String accessory;
-    private String mbti;
-    private String hobby;
-    private String secretTip;
-    private String tmi;
 }
