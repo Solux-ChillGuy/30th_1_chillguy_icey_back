9da4660fc6fa27e1e9c625c30c7c2b77e3068b85
xyz987164 <xyz987164@gmail.com>
2025-08-01 19:49:02 +0900

    fix: frontend base url update

--- icey/src/main/java/com/project/icey/global/config/WebConfig.java
+++ icey/src/main/java/com/project/icey/global/config/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://icey-backend-1027532113913.asia-northeast3.run.app","http://localhost:8080","http://localhost:5173") // 허용할 정확한 origin
+                .allowedOrigins("https://icey-backend-1027532113913.asia-northeast3.run.app","http://localhost:8080","http://localhost:5173","https://52.64.218.211") // 허용할 정확한 origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowCredentials(true);
     }
