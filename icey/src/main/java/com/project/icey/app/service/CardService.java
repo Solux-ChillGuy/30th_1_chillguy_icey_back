666078ac87b4fdf59c8d36647455e19299cffef9
yoonjae-kwon03 <yoonjae.kwon03@gmail.com>
2025-08-01 15:44:20 +0900

    fix: 악세사리 enum 한영 변환

--- icey/src/main/java/com/project/icey/app/service/CardService.java
+++ icey/src/main/java/com/project/icey/app/service/CardService.java
@@ -18,7 +18,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
-import static com.project.icey.app.domain.AccessoryType.동물;
+import static com.project.icey.app.domain.AccessoryType.*;
+
 
 @Service
 @RequiredArgsConstructor
@@ -210,7 +211,7 @@ public class CardService {
         init.setHobby("독서");
         init.setSecretTip("사실 수면 시간이 12시간이에요");
         init.setTmi("소싯적 휘파람 챔피언(교정하고 몰락함)");
-        init.setAccessory(동물);
+        init.setAccessory(BASIC);
 
         Card tpl = toEntity(init);
         tpl.setUser(user);
